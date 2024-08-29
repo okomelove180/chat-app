ffed37ea1f9a49909d6993a8ef42123ea2779485
okomelove180 <toru.camp180@gmail.com>
2024-08-29 15:25:18 +0900

    messagesテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -8,4 +6,5 @@ class User < ApplicationRecord
   
   has_many :room_users
   has_many :rooms, through: :room_users
+  has_many :messages
 end
