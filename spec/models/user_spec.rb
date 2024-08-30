2d3fda017bd9f31591760c4c762a48d8c5caeabb
okomelove180 <toru.camp180@gmail.com>
2024-08-30 20:49:43 +0900

    結合テストの実装

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -46,18 +46,18 @@ RSpec.describe User, type: :model do
         @user.password_confirmation = '1234567'
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
-       end
-       it '重複したemailが存在する場合は登録できない' do
+      end
+      it '重複したemailが存在する場合は登録できない' do
         @user.save
         another_user = FactoryBot.build(:user, email: @user.email)
         another_user.valid?
         expect(another_user.errors.full_messages).to include('Email has already been taken')
-       end
-       it 'emailは@を含まないと登録できない' do
+      end
+      it 'emailは@を含まないと登録できない' do
         @user.email = 'testmail'
         @user.valid?
         expect(@user.errors.full_messages).to include('Email is invalid')
-       end
+      end
     end
   end
 end
